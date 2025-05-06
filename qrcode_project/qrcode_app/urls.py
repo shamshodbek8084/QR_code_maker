5bcc424a404eebc8c480dbd3a2f927a052227037
shamshodbek8084 <erkinmulkamonov@gmail.com>
2025-05-07 01:22:25 +0500

    Loyixa o'z nihoyasiga yetdi

--- qrcode_project/qrcode_app/urls.py
+++ qrcode_project/qrcode_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('download/<str:data>/', views.download_qr, name='download_qr'),
+    path('download/<path:data>/', views.download_qr, name='download_qr'),
 ]
--- qrcode_project/qrcode_app/views.py
+++ qrcode_project/qrcode_app/views.py
@@ -3,60 +3,64 @@ import qrcode
 from io import BytesIO
 import base64
 from django.http import HttpResponse
+from django.urls import reverse
+from urllib.parse import quote, unquote
 
 def index(request):
     qr_code_image = None
     download_link = None
+    data = request.POST.get('data', '')
 
-    if request.method == 'POST':
-        data = request.POST.get('data')  # Get data from the form
-        if data:
-            # Generate QR code
-            qr = qrcode.QRCode(
-                version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=10,
-                border=4,
-            )
-            qr.add_data(data)
-            qr.make(fit=True)
+    if request.method == 'POST' and data:
+        # QR kod yaratish
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
 
-            # Create an image
-            img = qr.make_image(fill_color="black", back_color="white")
-            buffer = BytesIO()
-            img.save(buffer)
-            img_data = buffer.getvalue()
+        # QR kod rasmini yaratish
+        img = qr.make_image(fill_color="black", back_color="white")
+        buffer = BytesIO()
+        img.save(buffer)
+        img_data = buffer.getvalue()
 
-            # Encode image for rendering
-            qr_code_image = base64.b64encode(img_data).decode()
+        # Rasmni Base64 formatiga o‘tkazish
+        qr_code_image = base64.b64encode(img_data).decode()
 
-            # Create a download link
-            download_link = base64.b64encode(img_data).decode()
+        # Yuklab olish havolasini yaratish
+        encoded_data = quote(data, safe='')  # URL'ni kodlash
+        download_link = reverse('download_qr', args=[encoded_data])
 
     return render(request, 'index.html', {
         'qr_code_image': qr_code_image,
         'download_link': download_link,
-        'data': request.POST.get('data', '')
+        'data': data
     })
 
 def download_qr(request, data):
-    # Generate the QR code for download
+    original_data = unquote(data)  # URL'ni asl holiga qaytarish
+
+    # QR kod yaratish
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(data)
+    qr.add_data(original_data)
     qr.make(fit=True)
 
-    # Create an image
+    # Rasm yaratish
     img = qr.make_image(fill_color="black", back_color="white")
     buffer = BytesIO()
     img.save(buffer)
     buffer.seek(0)
 
-    # Create HTTP response to download the QR code
+    # HTTP javobi (QR kodni yuklab olish uchun)
     response = HttpResponse(buffer, content_type="image/png")
-    response['Content-Disposition'] = f'attachment; filename="qrcode.png"'
+    response['Content-Disposition'] = 'attachment; filename="qrcode.png"'
     return response
